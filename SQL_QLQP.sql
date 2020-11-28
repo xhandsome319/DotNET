525d9307beb1e642418e4760dc3f13e81e73ddc4
shasakiken <bautroixanhay03@gmail.com>
2020-11-28 17:22:44 +0700

    toi la ai

--- SQL_QLQP.sql
+++ SQL_QLQP.sql
@@ -4,6 +4,7 @@ GO
 USE QuanLyQuanPho
 GO
 
+
 -- Food
 -- Table
 -- FoodCategory
@@ -70,3 +71,161 @@ CREATE TABLE BillInfo
 )
 GO
 
+DBCC CHECKIDENT ('dbo.TableFood', RESEED, 0)
+DECLARE @i INT = 1
+
+--thêm bàn
+WHILE @i <= 10
+BEGIN
+	INSERT dbo.TableFood ( name)VALUES  ( N'Bàn ' + CAST(@i AS nvarchar(100)))
+	SET @i = @i + 1
+END
+ 
+
+DELETE DBO.TableFood
+SELECT * FROM DBO.TableFood
+
+-- thêm category
+INSERT dbo.FoodCategory
+        ( name )
+VALUES  ( N'Thức ăn nước'  -- name - nvarchar(100)
+          )
+INSERT dbo.FoodCategory
+        ( name )
+VALUES  ( N'Thức ăn khô'  
+          )
+INSERT dbo.FoodCategory
+        ( name )
+VALUES  ( N'Nước uống' )
+INSERT dbo.FoodCategory
+        ( name )
+VALUES  ( N'Thức ăn thêm' )
+
+-- thêm món ăn
+INSERT dbo.Food
+        ( name, idCategory, price )
+VALUES  ( N'Phở đặc biệt', -- name - nvarchar(100)
+          1, -- idCategory - int
+          39000)
+INSERT dbo.Food
+        ( name, idCategory, price )
+VALUES  ( N'Phở tái', 1, 35000)
+INSERT dbo.Food
+        ( name, idCategory, price )
+VALUES  ( N'Phở gà ', 1, 30000)
+INSERT dbo.Food
+        ( name, idCategory, price )
+VALUES  ( N'Phở bò chín', 1, 35000)
+INSERT dbo.Food
+        ( name, idCategory, price )
+VALUES  ( N'Phở bò viên ', 1, 30000)
+
+INSERT dbo.Food
+        ( name, idCategory, price )
+VALUES  ( N'Phở cuốn ', 2, 30000)
+INSERT dbo.Food
+        ( name, idCategory, price )
+VALUES  ( N'Phở chiên phồng ', 2, 30000)
+INSERT dbo.Food
+        ( name, idCategory, price )
+VALUES  ( N'Phở chiên trứng ', 2, 30000)
+INSERT dbo.Food
+        ( name, idCategory, price )
+VALUES  ( N'chả tôm ', 2, 30000)
+
+
+INSERT dbo.Food
+        ( name, idCategory, price )
+VALUES  ( N'7Up', 3, 12000)
+INSERT dbo.Food
+        ( name, idCategory, price )
+VALUES  ( N'Sting', 3, 12000)
+INSERT dbo.Food
+        ( name, idCategory, price )
+VALUES  ( N'Trà đá', 3, 2000)
+
+INSERT dbo.Food
+        ( name, idCategory, price )
+VALUES  ( N'Bánh phở', 4, 8000)
+INSERT dbo.Food
+        ( name, idCategory, price )
+VALUES  ( N'Bò', 4, 8000)
+INSERT dbo.Food
+        ( name, idCategory, price )
+VALUES  ( N'Bò viên', 4, 8000)
+
+-- thêm bill
+INSERT	dbo.Bill
+        ( DateCheckIn ,
+          DateCheckOut ,
+          idTable ,
+          status
+        )
+VALUES  ( GETDATE() , -- DateCheckIn - date
+          NULL , -- DateCheckOut - date
+          1 , -- idTable - int
+          0  -- status - int
+        )
+        
+INSERT	dbo.Bill
+        ( DateCheckIn ,
+          DateCheckOut ,
+          idTable ,
+          status
+        )
+VALUES  ( GETDATE() , -- DateCheckIn - date
+          NULL , -- DateCheckOut - date
+          2, -- idTable - int
+          0  -- status - int
+        )
+INSERT	dbo.Bill
+        ( DateCheckIn ,
+          DateCheckOut ,
+          idTable ,
+          status
+        )
+VALUES  ( GETDATE() , -- DateCheckIn - date
+          GETDATE() , -- DateCheckOut - date
+          3 , -- idTable - int
+          1  -- status - int
+        )
+
+-- thêm bill info
+INSERT	dbo.BillInfo
+        ( idBill, idFood, count )
+VALUES  ( 5, -- idBill - int
+          1, -- idFood - int
+          2  -- count - int
+          )
+INSERT	dbo.BillInfo
+        ( idBill, idFood, count )
+VALUES  ( 5, -- idBill - int
+          3, -- idFood - int
+          4  -- count - int
+          )
+INSERT	dbo.BillInfo
+        ( idBill, idFood, count )
+VALUES  ( 5, -- idBill - int
+          5, -- idFood - int
+          1  -- count - int
+          )
+INSERT	dbo.BillInfo
+        ( idBill, idFood, count )
+VALUES  ( 6, -- idBill - int
+          1, -- idFood - int
+          2  -- count - int
+          )
+INSERT	dbo.BillInfo
+        ( idBill, idFood, count )
+VALUES  ( 6, -- idBill - int
+          6, -- idFood - int
+          2  -- count - int
+          )
+INSERT	dbo.BillInfo
+        ( idBill, idFood, count )
+VALUES  ( 7, -- idBill - int
+          5, -- idFood - int
+          2  -- count - int
+          )         
+          
+GO
